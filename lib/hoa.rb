655bafd58db537c4ed014e9677b75654e57e9f5e
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-02 15:55:13 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,8 +8,7 @@ BASE_HOA = {
 def add_character(show, name)
   show = {
     :chipmunks => ["Alvin", "Simon", "Theodore"],
-    :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
-    :jetsons => ["George", "Jane", "Judy", "Elroy"]
+
   }
   
   show[:chipmunks] << "Dave"
